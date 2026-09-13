27594389e17a1c382fbd2535e2fa108abec3152d
gyutaetae <zn7034@naver.com>
2026-09-13 22:51:55 +0900

    Initial commit

---
+++ src/main/java/com/gyutae/financialledger/ReliableFinancialLedgerLabApplication.java
@@ -0,0 +1,13 @@
+package com.gyutae.financialledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliableFinancialLedgerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliableFinancialLedgerLabApplication.class, args);
+	}
+
+}
