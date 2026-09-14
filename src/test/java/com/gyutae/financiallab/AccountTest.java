fae73b643fc34cf03e501657bd48f97882b8127f
gyutaetae <zn7034@naver.com>
2026-09-14 12:42:44 +0900

    InsufficientBalanceException Test

--- src/test/java/com/gyutae/financiallab/AccountTest.java
+++ src/test/java/com/gyutae/financiallab/AccountTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 import com.gyutae.financiallab.account.domain.Account;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import com.gyutae.financiallab.account.domain.InsufficientBalanceException;
 
 public class AccountTest {
     @Test
@@ -18,5 +19,45 @@ public class AccountTest {
         account.deposit(new BigDecimal("3000"));
         //then
         assertThat(account.getBalance()).isEqualTo(new BigDecimal("3000"));
+    }
+
+    @Test
+    void initialbalance(){
+        //given
+        Account account = new Account();
+        //when
+        BigDecimal balance = account.getBalance();
+        //then
+        assertThat(balance).isEqualTo(new BigDecimal("0"));
+    }
+
+    @Test 
+    void doubleDeposit(){
+        Account account = new Account();
+        account.deposit(new BigDecimal("3000"));
+        account.deposit(new BigDecimal("5000"));
+        assertThat(account.getBalance()).isEqualTo(new BigDecimal("8000"));
+    }
+
+    @Test 
+    void withdraw(){
+        Account account = new Account();
+        account.deposit( new BigDecimal("10000"));
+        account.withdraw(new BigDecimal("3000"));
+        assertThat(account.getBalance()).isEqualTo(new BigDecimal("7000"));
+    }
+
+    @Test 
+    void InsufficientBalanceException(){
+        //given
+        Account account = new Account();
+        account.deposit(new BigDecimal("10000"));
+        //when
+        assertThatThrownBy(
+            () -> account.withdraw(new BigDecimal("13000"))
+        ).isInstanceOf(InsufficientBalanceException.class);
+
+        //then
+        assertThat(account.getBalance()).isEqualByComparingTo(new BigDecimal("10000"));
     }
 }
