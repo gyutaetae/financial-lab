b9d38142a9b478f704a8528f36ce6d49ed69685e
gyu <zn7034@naver.com>
2026-09-15 16:32:43 +0900

    AccountService.java

--- src/main/java/com/gyutae/financiallab/account/api/AccountController.java
+++ src/main/java/com/gyutae/financiallab/account/api/AccountController.java
@@ -1,5 +1,5 @@
 package com.gyutae.financiallab.account.api;
 
 public class AccountController {
-    
+
 }
--- src/main/java/com/gyutae/financiallab/account/application/AccountService.java
+++ src/main/java/com/gyutae/financiallab/account/application/AccountService.java
@@ -1,20 +1,24 @@
 package com.gyutae.financiallab.account.application;
 
-import org.springframework.stereotype.Service;
-import com.gyutae.financiallab.account.domain.Account;
-import java.util.Map;
+import java.math.BigDecimal;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import com.gyutae.financiallab.account.domain.Account;
 
 //application: 계좌 생성·조회 같은 업무 흐름
-//실제 api에서는 사용자가 만들지않고 서비스가 자동으로 ID를 생성함 
-@Service //이걸붙이면 new AccountService() 안해도 스프링이 알아서 만들어줌
+//실제 api에서는 사용자가 만들지않고 서비스가 자동으로 ID를 생성함
+@Service // 이걸붙이면 new AccountService() 안해도 스프링이 알아서 만들어줌
 public class AccountService {
-    Map<Long,Account> accounts = new ConcurrentHashMap<>(); //동시성 문제를 해결하기 위해 ConcurrentHashMap 사용
+	private final AtomicLong idGenerator = new AtomicLong(1);
+	private final ConcurrentHashMap<Long, Account> accounts = new ConcurrentHashMap<>();
 
-    public Account createAccount(BigDecial, initialBalance){
-        Account
-    }
+	public Account createAccount(BigDecimal initialBalance) {
+		Long id = idGenerator.getAndIncrement();
+		Account account = new Account(id, initialBalance);
+		accounts.put(id, account);
+		return account;
+	}
 }
-
-
-
