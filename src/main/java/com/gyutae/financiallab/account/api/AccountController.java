ea5f586b212cf10d0e95c4f65543740edbd31c2c
gyutaetae <zn7034@naver.com>
2026-09-17 04:03:49 +0900

    return response

--- src/main/java/com/gyutae/financiallab/account/api/AccountController.java
+++ src/main/java/com/gyutae/financiallab/account/api/AccountController.java
@@ -1,32 +1,41 @@
 package com.gyutae.financiallab.account.api;
+// controller는 처음에 http로 요청을받으면 json으로 변환해서 request에게 보냄
+
+import java.math.BigDecimal;
+
+// 컨트롤러는 서비스가 필요한데 직접만들지않고 에너테이션을 사용해서 스프링이 만들어서 생성자로 보냄
+// 스프링아 클래스 컨트롤러 객체로만들어서 관리해줘
+// 컨트롤러는 계좌를 새성하기위해 service가필요함
+// 생성자 주입(DI)
+// post/accounts 를 이 메서드가 처리, 계좌를 새로만들것이므로 post
+// json request를 createaccountrequest를 전달받고 accountresponse를 보낸다
+// acount에서 값을꺼내 response생성한다
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// controller는 처음에 http로 요청을받으면 json으로 변환해서 request에게 보냄 
-// 컨트롤러는 서비스가 필요한데 직접만들지않고 에너테이션을 사용해서 스프링이 만들어서 생성자로 보냄 
 import com.gyutae.financiallab.account.application.AccountService;
 import com.gyutae.financiallab.account.domain.Account;
 
-@RestController // 스프링아 클래스 컨트롤러 객체로만들어서 관리해줘
+@RestController
 @RequestMapping("/accounts")
 public class AccountController {
-    private final AccountService accountService; // 컨트롤러는 계좌를 새성하기위해 service가필요함
+	private final AccountService accountService;
 
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    } // 생성자 주입(DI)
+	public AccountController(AccountService accountService) {
+		this.accountService = accountService;
+	}
 
-    @PostMapping // post/accounts 를 이 메서드가 처리, 계좌를 새로만들것이므로 post
-    public AccountResponse createAccount(
-            @RequestBody CreateAccountRequest request // json request를 createaccountrequest를 전달받고 accountresponse를 보낸다
-    ) {
-        Account account = accountService.createAccount(request.initialBalance());
+	@PostMapping // post 요청이들어오면 메서드를 실행한다
+	public AccountResponse createAccount(
+			@RequestBody CreateAccountRequest request // json 요청을 java객체로 변경
+	) {
+		BigDecimal initialBalance = request.initialBalance();
+		Account account = accountService.createAccount(initialBalance); // service호출
 
-        return new AccountResponse(
-                account.getId(),
-                account.getBalance());
-    }
+		AccountResponse response = new AccountResponse(account.getId(), account.getBalance());
+		return response;
+	}
 }
